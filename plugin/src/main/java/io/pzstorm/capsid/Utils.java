a740ab453246d94e21042f5540b0e37e5138c157
Matthew Cain <yooksi@zoho.com>
2021-03-29 13:44:40 +0200

    Implement reading resource as text

--- plugin/src/main/java/io/pzstorm/capsid/Utils.java
+++ plugin/src/main/java/io/pzstorm/capsid/Utils.java
@@ -17,6 +17,33 @@
  */
 package io.pzstorm.capsid;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Utils {
 
+	/**
+	 * Reads the resource for given path and converts it to {@code String}.
+	 *
+	 * @param clazz {@code Class} to get the {@code ClassLoader} for.
+	 * @param path path to the resource to read.
+	 *
+	 * @throws IOException if unable to find resource for given path or an I/O error
+	 * 		occurred while retrieving resource as stream for class loader.
+	 */
+	public static String readResourceAsTextFromStream(Class<?> clazz, String path) throws IOException {
+
+		try (InputStream inputStream = clazz.getClassLoader().getResourceAsStream(path))
+		{
+			if (inputStream == null) {
+				throw new IOException("Unable to find resource for path '" + path + '\'');
+			}
+			Stream<String> stream = new BufferedReader(new InputStreamReader(inputStream)).lines();
+			return stream.collect(Collectors.joining("\n"));
+		}
+	}
 }
