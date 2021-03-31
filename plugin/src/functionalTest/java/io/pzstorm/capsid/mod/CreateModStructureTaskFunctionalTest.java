d807de0195d41cc5c203d9954e65f6ab32c18bfc
Matthew Cain <yooksi@zoho.com>
2021-03-31 14:47:30 +0200

    Test excluding dirs from mod structure

--- plugin/src/functionalTest/java/io/pzstorm/capsid/mod/CreateModStructureTaskFunctionalTest.java
+++ plugin/src/functionalTest/java/io/pzstorm/capsid/mod/CreateModStructureTaskFunctionalTest.java
@@ -18,6 +18,8 @@
 package io.pzstorm.capsid.mod;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.*;
 
 import org.gradle.testkit.runner.BuildResult;
@@ -45,6 +47,45 @@ class CreateModStructureTaskFunctionalTest extends PluginFunctionalTest {
 
 		Set<String> expectedDirNames = new HashSet<>();
 		Arrays.stream(gameDir.convert().resolve("media").toFile().listFiles(File::isDirectory))
+				.forEach(f -> expectedDirNames.add(f.getName()));
+
+		Set<String> actualDirNames = new HashSet<>();
+		Arrays.stream(getProjectDir().toPath().resolve("media").toFile().listFiles(File::isDirectory))
+				.forEach(f -> actualDirNames.add(f.getName()));
+
+		Assertions.assertEquals(expectedDirNames, actualDirNames);
+	}
+
+	@Test
+	void shouldExcludedResourceSrcDirsFromModStructure() throws IOException {
+
+		Set<String> excludedSrcDirs = new HashSet<>(Arrays.asList(
+				"media/luaexamples", "media/newuitests", "media/launcher"
+		));
+		writeToProjectFile("build.gradle", new String[] {
+				"plugins {",
+				"	id('io.pzstorm.capsid')",
+				"}",
+				"",
+				"capsid.excludeResourceDirs " +
+				String.format("\t'%s'", String.join("', '", excludedSrcDirs)),
+		});
+		GradleRunner runner = getRunner();
+		List<String> arguments = new ArrayList<>(runner.getArguments());
+		arguments.add(ModTasks.CREATE_MOD_STRUCTURE.name);
+
+		UnixPath gameDir = Objects.requireNonNull(LocalProperties.GAME_DIR.findProperty(getProject()));
+		File gameDirFile = gameDir.convert().toFile();
+
+		for (String excludedSrcDirName : excludedSrcDirs) {
+			Files.createDirectory(new File(gameDirFile, excludedSrcDirName).toPath());
+		}
+		BuildResult result = getRunner().withArguments(arguments).build();
+		assertTaskOutcomeSuccess(result, ModTasks.CREATE_MOD_STRUCTURE.name);
+
+		Set<String> expectedDirNames = new HashSet<>();
+		Arrays.stream(gameDirFile.toPath().resolve("media").toFile().listFiles(File::isDirectory))
+				.filter(f -> !excludedSrcDirs.contains("media/" + f.getName()))
 				.forEach(f -> expectedDirNames.add(f.getName()));
 
 		Set<String> actualDirNames = new HashSet<>();
