54a06a7787c2c0271584d931103642be3f2f3d7d
Matthew Cain <yooksi@zoho.com>
2021-04-04 23:03:11 +0200

    Test assembling game class jar archives

---
+++ plugin/src/functionalTest/java/io/pzstorm/capsid/zomboid/task/ZomboidJarTaskFunctionalTest.java
@@ -0,0 +1,82 @@
+/*
+ * Storm Capsid - Project Zomboid mod development framework for Gradle.
+ * Copyright (C) 2021 Matthew Cain
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package io.pzstorm.capsid.zomboid.task;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.io.MoreFiles;
+
+import org.gradle.testkit.runner.BuildResult;
+import org.gradle.testkit.runner.GradleRunner;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import io.pzstorm.capsid.CapsidPlugin;
+import io.pzstorm.capsid.PluginFunctionalTest;
+import io.pzstorm.capsid.util.Utils;
+
+class ZomboidJarTaskFunctionalTest extends PluginFunctionalTest {
+
+	@Test
+	void shouldAssembleJarArchiveContainingGameClasses() throws IOException {
+
+		File source = CapsidPlugin.getGameDirProperty(getProject());
+		Assertions.assertTrue(CapsidPlugin.getZomboidClassesDir(getProject()).mkdirs());
+
+		File destination = new File(getProject().getProjectDir(), "lib");
+		Assertions.assertTrue(destination.mkdirs());
+
+		Set<String> filesToInclude = ImmutableSet.of(
+				"class1.class", "class2.class", "class3.class"
+		);
+		File dummyClass = Utils.getFileFromResources("dummy.class");
+		for (String include : filesToInclude) {
+			com.google.common.io.Files.copy(dummyClass, new File(source, include));
+		}
+		Map<String, File> filesToExclude = ImmutableMap.of(
+				"textFile.txt", Utils.getFileFromResources("dummy.txt"),
+				"imageFile.png", Utils.getFileFromResources("dummy.png")
+		);
+		for (Map.Entry<String, File> entry : filesToExclude.entrySet()) {
+			com.google.common.io.Files.copy(entry.getValue(), new File(source, entry.getKey()));
+		}
+		GradleRunner runner = getRunner();
+		List<String> arguments = new ArrayList<>(runner.getArguments());
+		arguments.add(ZomboidTasks.ZOMBOID_JAR.name);
+
+		BuildResult result = getRunner().withArguments(arguments).build();
+		assertTaskOutcomeSuccess(result, ZomboidTasks.ZOMBOID_JAR.name);
+
+		File resultJar = java.nio.file.Files.walk(destination.toPath())
+				.filter(f -> MoreFiles.getFileExtension(f).equals("jar"))
+				.findAny().orElseThrow(RuntimeException::new).toFile();
+
+		Utils.unzipArchive(resultJar, destination);
+
+		for (String include : filesToInclude) {
+			Assertions.assertTrue(new File(destination, include).exists());
+		}
+		for (Map.Entry<String, File> entry : filesToExclude.entrySet()) {
+			Assertions.assertFalse(new File(destination, entry.getKey()).exists());
+		}
+	}
+}
