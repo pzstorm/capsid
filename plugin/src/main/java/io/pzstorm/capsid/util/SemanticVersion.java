d66e61fffc9045e1b0cd991a4f438c49ad9c78de
Matthew Cain <yooksi@zoho.com>
2021-04-07 01:31:36 +0200

    Create semantic version class

---
+++ plugin/src/main/java/io/pzstorm/capsid/util/SemanticVersion.java
@@ -0,0 +1,60 @@
+/*
+ * Storm Capsid - Project Zomboid mod development framework for Gradle.
+ * Copyright (C) 2021 Matthew Cain
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package io.pzstorm.capsid.util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.gradle.api.InvalidUserDataException;
+
+/**
+ * This object represents a valid semantic version.
+ *
+ * @see <a href="https://semver.org/">Semantic Versioning</a>
+ */
+public class SemanticVersion {
+
+	private static final Pattern SEM_VER = Pattern.compile("^(\\d+)\\.(\\d+)\\.(\\d+)(-(.*))?$");
+
+	public final Integer major, minor, patch;
+	public final String classifier;
+
+	/**
+	 * Creates a new semantic version instance from given {@code String}.
+	 *
+	 * @throws InvalidUserDataException if given semantic version is malformed.
+	 */
+	public SemanticVersion(String version) {
+
+		Matcher matcher = SEM_VER.matcher(version);
+		if (!matcher.find()) {
+			throw new InvalidUserDataException("Malformed semantic version '" + version + '\'');
+		}
+		this.major = Integer.valueOf(matcher.group(1));
+		this.minor = Integer.valueOf(matcher.group(2));
+		this.patch = Integer.valueOf(matcher.group(3));
+
+		String sClassifier = matcher.group(4);
+		this.classifier = sClassifier != null ? matcher.group(5) : "";
+	}
+
+	@Override
+	public String toString() {
+		return major + '.' + minor + '.' + patch + classifier;
+	}
+}
