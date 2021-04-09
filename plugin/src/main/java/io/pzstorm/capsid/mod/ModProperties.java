3f24f5722d2787e9f50e57b2a2bb99fac3266f4a
Matthew Cain <yooksi@zoho.com>
2021-04-09 07:21:59 +0200

    Declare additional mod properties

--- plugin/src/main/java/io/pzstorm/capsid/mod/ModProperties.java
+++ plugin/src/main/java/io/pzstorm/capsid/mod/ModProperties.java
@@ -21,6 +21,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
 import java.net.URL;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Set;
 
@@ -33,6 +34,7 @@ import org.jetbrains.annotations.Unmodifiable;
 import io.pzstorm.capsid.CapsidPlugin;
 import io.pzstorm.capsid.property.CapsidProperties;
 import io.pzstorm.capsid.property.CapsidProperty;
+import io.pzstorm.capsid.util.SemanticVersion;
 
 /**
  * This class represents properties from {@code mod.info} file.
@@ -48,14 +50,29 @@ public class ModProperties extends CapsidProperties {
 	public static final CapsidProperty<String> MOD_NAME;
 
 	/**
+	 * Path to image that represents this mod.
+	 */
+	public static final CapsidProperty<Path> MOD_POSTER;
+
+	/**
 	 * Mod description visible in game main menu.
 	 */
 	public static final CapsidProperty<String> MOD_DESCRIPTION;
 
 	/**
+	 * Unique string that identifies this mod.
+	 */
+	public static final CapsidProperty<String> MOD_ID;
+
+	/**
 	 * Hyperlink pointing to a project repository or workshop page.
 	 */
 	public static final CapsidProperty<URL> MOD_URL;
+
+	/**
+	 * Current mod project semantic version.
+	 */
+	public static final CapsidProperty<SemanticVersion> MOD_VERSION;
 
 	/**
 	 * Latest version of Project Zomboid compatible with this mod.
@@ -69,15 +86,30 @@ public class ModProperties extends CapsidProperties {
 				.withDefaultValue("Project Zomboid mod")
 				.build();
 
+		MOD_POSTER = new CapsidProperty.Builder<>("mod.poster", Path.class)
+				.withEnvironmentVar("MOD_POSTER")
+				.withDefaultValue(Paths.get("poster.png"))
+				.isRequired(false)
+				.build();
+
 		MOD_DESCRIPTION = new CapsidProperty.Builder<>("mod.desc", String.class)
 				.withEnvironmentVar("MOD_DESC")
 				.withDefaultValue("No description available.")
 				.isRequired(false)
 				.build();
 
+		MOD_ID = new CapsidProperty.Builder<>("mod.id", String.class)
+				.withEnvironmentVar("MOD_ID")
+				.build();
+
 		MOD_URL = new CapsidProperty.Builder<>("mod.url", URL.class)
 				.withEnvironmentVar("MOD_URL")
 				.isRequired(false)
+				.build();
+
+		MOD_VERSION = new CapsidProperty.Builder<>("mod.version", SemanticVersion.class)
+				.withEnvironmentVar("MOD_VERSION")
+				.withDefaultValue(new SemanticVersion("0.1.0"))
 				.build();
 
 		//noinspection SpellCheckingInspection
