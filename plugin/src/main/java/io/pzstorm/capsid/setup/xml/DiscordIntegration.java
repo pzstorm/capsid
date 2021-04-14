b943e63826a5f2d56901fb6ba963fe2ff99221f1
Matthew Cain <yooksi@zoho.com>
2021-04-14 17:57:49 +0200

    Configure Discord integration

--- plugin/src/main/java/io/pzstorm/capsid/setup/xml/DiscordIntegration.java
+++ plugin/src/main/java/io/pzstorm/capsid/setup/xml/DiscordIntegration.java
@@ -17,6 +17,20 @@
  */
 package io.pzstorm.capsid.setup.xml;
 
+import java.nio.file.Paths;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+
+import org.gradle.api.Project;
+import org.jetbrains.annotations.Nullable;
+import org.w3c.dom.Element;
+
+import io.pzstorm.capsid.mod.ModProperties;
+
+/**
+ * This class represents an {@code XML} configuration for Discord integration.
+ */
 public class DiscordIntegration extends XMLDocument {
 
 	public static final DiscordIntegration INTEGRATION = new DiscordIntegration();
@@ -25,5 +39,62 @@ public class DiscordIntegration extends XMLDocument {
 		super("discord", Paths.get(".idea"));
 	}
 
-	// TODO: finish writing this after implementing mod.gradle
+	@Override
+	protected Transformer createAndConfigureTransformer() throws TransformerConfigurationException {
+
+		Transformer transformer = super.createAndConfigureTransformer();
+
+		transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
+		transformer.setOutputProperty(OutputKeys.DOCTYPE_PUBLIC, "");
+
+		return transformer;
+	}
+
+	@Override
+	public DiscordIntegration configure(Project project) {
+
+		// <project version="4">
+		Element projectElement = document.createElement("project");
+
+		projectElement.setAttribute("version", "4");
+		appendOrReplaceRootElement(projectElement);
+
+		// <component name="DiscordProjectSettings">
+		Element component = document.createElement("component");
+
+		component.setAttribute("name", "DiscordProjectSettings");
+		projectElement.appendChild(component);
+
+		// <option name="show" value="PROJECT" />
+		Element optionShow = document.createElement("option");
+
+		optionShow.setAttribute("name", "show");
+		optionShow.setAttribute("value", "PROJECT");
+		component.appendChild(optionShow);
+
+		// <option name="nameOverrideEnabled" value="true" />
+		Element optionOverrideEnabled = document.createElement("option");
+
+		optionOverrideEnabled.setAttribute("name", "nameOverrideEnabled");
+		optionOverrideEnabled.setAttribute("value", "true");
+		component.appendChild(optionOverrideEnabled);
+
+		// <option name="nameOverrideText" value="<projectName>" />
+		@Nullable String projectName = (String) project.findProperty(ModProperties.MOD_NAME.name);
+		Element optionOverrideText = document.createElement("option");
+
+		optionOverrideText.setAttribute("name", "nameOverrideText");
+		optionOverrideText.setAttribute("value", projectName != null ? projectName : "PZ Mod");
+		component.appendChild(optionOverrideText);
+
+		// <option name="description" value="<projectDescription>" />
+		@Nullable String projectDesc = (String) project.findProperty(ModProperties.MOD_DESCRIPTION.name);
+		Element optionDescription = document.createElement("option");
+
+		optionDescription.setAttribute("name", "description");
+		optionDescription.setAttribute("value", projectDesc != null ? projectDesc : "Project Zomboid mod.");
+		component.appendChild(optionDescription);
+
+		return (DiscordIntegration) super.configure(project);
+	}
 }
