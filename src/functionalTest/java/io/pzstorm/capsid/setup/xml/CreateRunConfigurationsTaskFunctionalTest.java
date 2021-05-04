d5bafb9779ce615fe8482fae36571656b159aaa6
Matthew <yooks@tuta.io>
2021-05-04 17:53:33 +0200

    Test launch run configs on Linux

--- src/functionalTest/java/io/pzstorm/capsid/setup/xml/CreateRunConfigurationsTaskFunctionalTest.java
+++ src/functionalTest/java/io/pzstorm/capsid/setup/xml/CreateRunConfigurationsTaskFunctionalTest.java
@@ -22,6 +22,7 @@ import java.io.IOException;
 import java.util.Map;
 
 import io.pzstorm.capsid.CapsidGradleRunner;
+import org.gradle.internal.os.OperatingSystem;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -35,12 +36,12 @@ class CreateRunConfigurationsTaskFunctionalTest extends PluginFunctionalTest {
 
 	private static final ImmutableMap<XMLDocument, String> RUN_CONFIGS =
 			ImmutableMap.<XMLDocument, String>builder()
-					.put(LaunchRunConfig.RUN_ZOMBOID, "Run_Zomboid.xml")
-					.put(LaunchRunConfig.RUN_ZOMBOID_LOCAL, "Run_Zomboid_local.xml")
-					.put(LaunchRunConfig.DEBUG_ZOMBOID, "Debug_Zomboid.xml")
-					.put(LaunchRunConfig.DEBUG_ZOMBOID_LOCAL, "Debug_Zomboid_local.xml")
-					.put(GradleRunConfig.INITIALIZE_MOD, "initializeMod.xml")
-					.put(GradleRunConfig.SETUP_WORKSPACE, "setupWorkspace.xml")
+					.put(LaunchRunConfig.RUN_ZOMBOID, "Run_Zomboid")
+					.put(LaunchRunConfig.RUN_ZOMBOID_LOCAL, "Run_Zomboid_local")
+					.put(LaunchRunConfig.DEBUG_ZOMBOID, "Debug_Zomboid")
+					.put(LaunchRunConfig.DEBUG_ZOMBOID_LOCAL, "Debug_Zomboid_local")
+					.put(GradleRunConfig.INITIALIZE_MOD, "initializeMod")
+					.put(GradleRunConfig.SETUP_WORKSPACE, "setupWorkspace")
 					.build();
 
 	CreateRunConfigurationsTaskFunctionalTest() {
@@ -59,10 +60,15 @@ class CreateRunConfigurationsTaskFunctionalTest extends PluginFunctionalTest {
 		for (Map.Entry<XMLDocument, String> entry : RUN_CONFIGS.entrySet())
 		{
 			String filename = entry.getValue();
-			File runConfig = new File(runConfigurations, filename);
+			File runConfig = new File(runConfigurations, filename + ".xml");
 			Assertions.assertTrue(runConfig.exists());
 
-			String expected = Utils.readResourceAsTextFromStream(getClass(), filename);
+			String resourceFilename = filename;
+			boolean isLinux = OperatingSystem.current() == OperatingSystem.LINUX;
+			if (isLinux && entry.getKey() instanceof LaunchRunConfig) {
+				resourceFilename += "_linux";
+			}
+			String expected = Utils.readResourceAsTextFromStream(getClass(), resourceFilename + ".xml");
 			Assertions.assertEquals(expected, Utils.readTextFromFile(runConfig));
 		}
 	}
